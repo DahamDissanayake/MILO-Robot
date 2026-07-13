b5e6d3be72583834948931acdd29e4a7068de667
Daham Dissanayake <68488077+DahamDissanayake@users.noreply.github.com>
2026-07-13 19:31:13 +0530

    feat(webapp): merge Ears and Voice into one Communication panel

--- bridge/milo_bridge/webapp/static/js/registry.js
+++ bridge/milo_bridge/webapp/static/js/registry.js
@@ -1,6 +1,7 @@
 // Adding a panel = create js/panels/<name>.js + add it to the right zone below.
 import camera from "./panels/camera.js";
 import move from "./panels/move.js";
+import comm from "./panels/comm.js";
 import sensors from "./panels/sensors.js";
 import graph from "./panels/graph.js";
 import poses from "./panels/poses.js";
@@ -8,7 +9,7 @@ import servos from "./panels/servos.js";
 import log from "./panels/log.js";
 
 export const registry = {
-  cockpitCenter: [camera, move],
+  cockpitCenter: [camera, move, comm],
   cockpitSide: [sensors],
   graph: [graph],
   tools: [poses, servos, log],
